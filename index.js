f8dd1b501e31cb45c3fe0d488536f34674b51d55
Batikan <60034174+Batikan-Kutluer@users.noreply.github.com>
2022-06-24 16:28:06 +0300

    Adet eklendi.

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ let http = require("http").createServer(app);
 const TradingView = require('@mathieuc/tradingview');
 const client = new TradingView.Client(); // Creates a websocket client
 
+const adet = require('./adet.json');
+
 let api = {};
 let dolar = 0;
 
@@ -42,59 +44,55 @@ function hisse(_hisse = "GSRAY", callback) {
 
 hisse("FX:USDTRY", (data) => {
     dolar = data.close
-    api["USD"] = {
-        birim: "USD",
+
+    let birim = "USD";
+
+    api[birim] = {
+        birim: birim,
         alis: fix(data.open),
         satis: fix(data.close),
-        toplam: fix(290 * data.close)
+        adet: adet[birim],
+        toplam: fix(adet[birim] * data.close)
     }
 })
 
 hisse("BINANCE:FETUSDT", (data) => {
     if (dolar == 0) return;
 
-    api["FET"] = {
-        birim: "FET",
-        alis: fix(data.open * dolar),
-        satis: fix(data.close * dolar),
-        toplam: fix(0 * data.close)
+    let birim = "FET";
+
+    api[birim] = {
+        birim: birim,
+        alis: data.open,
+        satis: data.close,
+        adet: adet[birim],
+        toplam: fix(adet[birim] * data.close)
     }
 })
 
 hisse("BINANCE:BTCTRY", (data) => {
-    api["BTC"] = {
-        birim: "BTC",
+    let birim = "BTC";
+
+    api[birim] = {
+        birim: birim,
         alis: data.open,
         satis: data.close,
-        toplam: fix(0.02449323 * data.close)
+        adet: adet[birim],
+        toplam: fix(adet[birim] * data.close)
     }
 })
 
 let hisseList = ["GSRAY", "BJKAS", "TTKOM", "TCELL"];
 
-hisseList.forEach((item) => {
-    hisse(item, (data) => {
-        let tutar = 0;
+hisseList.forEach((birim) => {
+    hisse(birim, (data) => {
+        let tutar = adet[birim] || 0;
 
-        switch (item) {
-            case "GSRAY":
-                tutar = 902;
-                break
-            case "BJKAS":
-                tutar = 943;
-                break
-            case "TTKOM":
-                tutar = 196;
-                break
-            case "TCELL":
-                tutar = 165;
-                break
-        }
-
-        api[item] = {
-            birim: item,
+        api[birim] = {
+            birim: birim,
             alis: fix(data.open),
             satis: fix(data.close),
+            adet: adet[birim],
             toplam: fix(tutar * data.close)
         }
     })
